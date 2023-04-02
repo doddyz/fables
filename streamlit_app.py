a17d24a5277d1836fc3f33b505b7098ef3a1fe34
doddyz <doddyz@live.com>
2023-04-02 17:30:35 +0100

    Tabbed Branch v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,11 @@ st.set_page_config(
 
 st.title('Les Fables de La Fontaine')
 
+tab1, tab2, tab3 = st.tabs([f"**Livre 1**", f"**Livre 2**", f"**Livre 3**"])
+tab4, tab5, tab6 = st.tabs([f"**Livre 4**", f"**Livre 5**", f"**Livre 6**"])
+# tab1, tab2, tab3 = st.tabs([f"**Livre 1**", f"**Livre 2**", f"**Livre 3**"])
+# tab1, tab2, tab3 = st.tabs([f"**Livre 1**", f"**Livre 2**", f"**Livre 3**"])
 
-tab1, tab2, tab3 = st.tabs(["# Livre 1", "# Livre 2", "# Livre 3"])
 
 
 # fables_all_tocs()
@@ -29,12 +32,27 @@ tab1, tab2, tab3 = st.tabs(["# Livre 1", "# Livre 2", "# Livre 3"])
 # display_all_fables_all_books()
 
 with tab1:
+    fables_toc(1)
     display_fables_in_book(1)
 
 with tab2:
+    fables_toc(2)
     display_fables_in_book(2)
 
 with tab3:
+    fables_toc(3)
     display_fables_in_book(3)
+
+with tab4:
+    fables_toc(4)
+    display_fables_in_book(4)
+
+with tab5:
+    fables_toc(5)
+    display_fables_in_book(5)
+
+with tab6:
+    fables_toc(6)
+    display_fables_in_book(6)
 
 
